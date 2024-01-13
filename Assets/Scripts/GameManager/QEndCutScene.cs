4a5f3ed95edaa062246b7047729f5f1a16eee606
mally <malymaly0012@gmail.com>
2024-01-14 00:57:54 +0200

    Changing the portal out so it will open a selection menu to choose if you will want to stay in game or to end the game + adding a white screen for a second between the dialogue screen and the next

--- Assets/Scripts/GameManager/QEndCutScene.cs
+++ Assets/Scripts/GameManager/QEndCutScene.cs
@@ -7,12 +7,14 @@ using UnityEngine.SceneManagement;
 public class QEndCutScene : MonoBehaviour
 {
     private string EndScene;
-
+    public GameObject portal;
+    public AudioSource portalAudio;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bloom") || collision.gameObject.CompareTag("Stella") || collision.gameObject.CompareTag("Flora"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+          portalAudio.Play();
+          portal.SetActive(true);
 
         }
     }
